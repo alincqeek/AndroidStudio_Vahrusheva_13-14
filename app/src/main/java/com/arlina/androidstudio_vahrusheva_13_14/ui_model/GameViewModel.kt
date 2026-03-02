ae0c24fe990f7c404d9ce31aed5fbc3391e7c6c2
alincqeek <a72658695@gmail.com>
2026-03-02 19:05:33 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/arlina/androidstudio_vahrusheva_13_14/ui_model/GameViewModel.kt
+++ app/src/main/java/com/arlina/androidstudio_vahrusheva_13_14/ui_model/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.arlina.androidstudio_vahrusheva_13_14.ui_model
 import androidx.lifecycle.ViewModel
 import com.arlina.androidstudio_vahrusheva_13_14.data.GameUiState
+import com.arlina.androidstudio_vahrusheva_13_14.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,11 +11,34 @@ class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
     init {
         resetGame()
     }
-
-    fun resetGame() {
-
+    private fun shuffleCurrentWord(word: String): String{
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word){
+            tempWord.shuffle()
+        }
+        return String(tempWord)
     }
+
+    private fun pickRandomWordAndShuffle():String{
+        currentWord = allWords.random()
+        while(usedWords.contains(currentWord)){
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
+    }
+
+
 }
